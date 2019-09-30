fc9c54b3d38690cd6dc4018ab499f156612eea38
Jorge Luiz Baldan Soprani <jorge.soprani@prosperi.local>
2019-09-30 18:50:37 -0300

    A

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-acesso',
   templateUrl: './acesso.component.html',
-  styleUrls: ['./acesso.component.css']
+  styleUrls: ['./acesso.component.css'],
+  animations: [
+    trigger('animacao-banner', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(-50px, 0)'
+        }),
+        animate('500ms 0ms ease-in-out')
+      ])
+    ]),
+    trigger('animacao-painel', [
+      state('criado', style({
+        opacity: 1
+      })),
+      transition('void => criado', [
+        style({
+          opacity: 0,
+          transform: 'translate(50px, 0)'
+        }),
+        animate('500ms 0ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class AcessoComponent implements OnInit {
-
+  public estadoBanner: string = 'criado';
+  public estadoPainel: string = 'criado';
+  public cadastro: boolean = false;
   constructor() { }
 
   ngOnInit() {
   }
 
+  cadastreSe() {
+    this.cadastro = true;
+  }
 }
