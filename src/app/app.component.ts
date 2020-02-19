bbe51b8aef248af0d20ce32fe9d18381fd336ad1
Jorge Soprani <jorgesoprani@gmail.com>
2020-02-19 20:25:03 -0300

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() : void {
     firebase.initializeApp({
-      apiKey: "AIzaSyC5Gh98ZWD54J4InmOBwBKuSVkIuXe-PkI",
-      authDomain: "jta-instagram-c2fb2.firebaseapp.com",
-      databaseURL: "https://jta-instagram-c2fb2.firebaseio.com",
-      projectId: "jta-instagram-c2fb2",
-      storageBucket: "jta-instagram-c2fb2.appspot.com",
-      messagingSenderId: "818834945591",
-      appId: "1:818834945591:web:4b9095baa54832972771b2",
-      measurementId: "G-W7S83DPKHH"
+      //..
     });
   }
 }
